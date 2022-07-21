9d6b8890265def62f82cee058868a4c0e0d50aa2
Hibri Marzook <hibri@hibri.net>
2022-07-21 14:46:46 +0100

    anagrams Co-authored-by: sam-gibbons-contino <sam-gibbons-contino@users.noreply.github.com> Co-authored-by: Chris Clayton <ccgovuk@users.noreply.github.com>

--- first_test.go
+++ first_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"sort"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,5 +9,40 @@ import (
 
 func TestAlwaysTrue(t *testing.T) {
 
-	assert.True(t, false)
+	assert.True(t, true)
+}
+
+func TestKinshipPinkish(t *testing.T) {
+	result := solve("kinship")
+
+	assert.Contains(t, result, "pinkish")
+}
+
+func TestPoolLoopPolo(t *testing.T) {
+	result := solve("loop")
+
+	assert.Equal(t, len(result), 2)
+	assert.Contains(t, result, "polo")
+	assert.Contains(t, result, "pool")
+}
+
+func solve(word string) []string {
+	dictionary := []string{"pinkish", "polo", "pool"}
+	result := []string{}
+	x := runeArray(word)
+
+	for _, wordInDictonary := range dictionary {
+		if runeArray(wordInDictonary) == x {
+			result = append(result, wordInDictonary)
+		}
+	}
+	return result
+}
+
+func runeArray(word string) string {
+	runes := []rune(word)
+	sort.Slice(runes, func(i, j int) bool {
+		return runes[i] < runes[j]
+	})
+	return string(runes)
 }
