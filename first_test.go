ab3ac2e7c7957775cd29dc63211b7cd1f332b83f
Hibri Marzook <hibri@hibri.net>
2022-08-25 11:44:42 +0100

    Co-authored-by: danbower-contino <danbower-contino@users.noreply.github.com> Co-authored-by: Chris Clayton <ccgovuk@users.noreply.github.com>

--- first_test.go
+++ first_test.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"io"
+	"log"
+	"os"
 	"sort"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -13,26 +18,66 @@ func TestAlwaysTrue(t *testing.T) {
 }
 
 func TestKinshipPinkish(t *testing.T) {
-	result := solve("kinship")
+	//var dictionary = []string{"pinkish", "polo", "pool", "loop"}
+	var dictionary = readDictionary("/usr/share/dict/words")
+	result := solve("kinship", dictionary)
 
 	assert.Contains(t, result, "pinkish")
 }
 
 func TestPoolLoopPolo(t *testing.T) {
-	result := solve("loop")
+
+	var dictionary = []string{"pinkish", "polo", "pool", "loop"}
+
+	result := solve("loop", dictionary)
 
 	assert.Equal(t, len(result), 2)
 	assert.Contains(t, result, "polo")
 	assert.Contains(t, result, "pool")
 }
 
-func solve(word string) []string {
-	dictionary := []string{"pinkish", "polo", "pool"}
+func readDictionary(filename string) []string {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	slice := make([]string, 0)
+	reader := bufio.NewReader(file)
+	for {
+		str, err := reader.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+		slice = append(slice, strings.TrimSuffix(str, "\n"))
+	}
+	return slice
+
+}
+
+// func readToDisplayUsingFile1(f *os.File){
+//     defer f.Close()
+//     slice := make([]string,0)
+
+//     reader := bufio.NewReader(f)
+
+//     for{
+
+//     str, err := reader.ReadString('\n')
+//     if err == io.EOF{
+//         break
+//     }
+
+//	    slice = append(slice, str)
+//	}
+func solve(word string, dictionary []string) []string {
+
 	result := []string{}
 	x := runeArray(word)
 
 	for _, wordInDictonary := range dictionary {
-		if runeArray(wordInDictonary) == x {
+		if runeArray(wordInDictonary) == x && wordInDictonary != word {
 			result = append(result, wordInDictonary)
 		}
 	}
