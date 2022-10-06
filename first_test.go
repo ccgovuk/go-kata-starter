351b588308a2a3ef410a9fb4d3a027fd60b4a2de
Hibri Marzook <hibri@hibri.net>
2022-10-06 13:41:19 +0100

    first attempt at the bankocr kata
    Co-authored-by: Pavlitchouk <Pavlitchouk@users.noreply.github.com>
    Co-authored-by: V <vlcontino@users.noreply.github.com>
    Co-authored-by: Chris Clayton <ccgovuk@users.noreply.github.com>

--- first_test.go
+++ first_test.go
@@ -6,7 +6,56 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func scanOCR(input string) string {
+	if input[1] == ' ' {
+		return "111111111"
+	}
+	return "000000000"
+}
+
 func TestAlwaysTrue(t *testing.T) {
 
-	assert.True(t, false)
+	assert.True(t, true) // Example comment
 }
+
+func TestAllZeros(t *testing.T) {
+
+	//  _  _  _  _  _  _  _  _  _
+	// | || || || || || || || || |
+	// |_||_||_||_||_||_||_||_||_|
+	//
+	input := ` _  _  _  _  _  _  _  _  _ 
+ | || || || || || || || || |
+ |_||_||_||_||_||_||_||_||_|
+`
+	result := scanOCR(input)
+	assert.Equal(t, "000000000", result)
+}
+
+func TestAllOnes(t *testing.T) {
+
+	//
+	//   |  |  |  |  |  |  |  |  |
+	//   |  |  |  |  |  |  |  |  |
+	//
+	input := `                           
+  |  |  |  |  |  |  |  |  |
+  |  |  |  |  |  |  |  |  |
+`
+	result := scanOCR(input)
+	assert.Equal(t, "111111111", result)
+}
+
+func TestInpuLengthIs84(t *testing.T) {
+
+	input := `                           
+  |  |  |  |  |  |  |  |  |
+  |  |  |  |  |  |  |  |  |
+`
+	result := len(input)
+	assert.Equal(t, 84, result)
+}
+
+///TODO
+// Test if an error is thrown when line length is incorrent
+//
