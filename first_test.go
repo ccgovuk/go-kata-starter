81e4368b743d59eb4f1471fa82ffba9912cf2cc4
Chris Clayton <chris@viceroy.consulting>
2022-10-20 13:20:00 +0100

    20th October Changes
    Co-authored-by: V <vlcontino@users.noreply.github.com>
    Co-authored-by: Hibri Marzook <hibri+github@hibri.net>
    Co-authored-by: nigel-contino <nigel-contino@users.noreply.github.com>
    Co-authored-by: Bora Özkan <boraozkan1987@hotmail.com>

--- first_test.go
+++ first_test.go
@@ -1,28 +1,56 @@
 package main
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+var hashMap = map[string]string{
+	" _ | ||_|": "0",
+	"     |  |": "1",
+	" _  _||__": "2",
+	" _  _| _|": "3",
+	"   |_|  |": "4",
+	" _ |_||_|": "8",
+}
+
 func scanOCR(input string) string {
+	output := ""
+
 	if len(input) != 84 {
 		return " ERR"
 	}
-	switch {
-	case input[1] == ' ':
-		return "111111111"
-	case input[29] == '_':
-		return "222222222"
-	default:
-		return "000000000"
+
+	// switch {
+	// case input[1] == ' ':
+	// 	return "111111111"
+	// case input[29] == '_':
+	// 	return "222222222"
+	// default:
+	// 	return "000000000"
+	// }
+
+	// for i, character := range inputArray[0] {
+	// 	o := input[0:3]
+	// 	o := input[3:6]
+	// }
+
+	for i := 0; i < 27; i += 3 {
+		str1 := input[i : i+3]
+		str2 := input[i+28 : i+31]
+		str3 := input[i+56 : i+59]
+		digit := parseDigit([]string{str1, str2, str3})
+		output += digit
 	}
 
+	return output
 }
 
-func parseDigit(input [3]string) rune {
-	return '8'
+func parseDigit(input []string) string {
+	key := strings.Join(input, "")
+	return hashMap[key]
 }
 
 func TestAlwaysTrue(t *testing.T) {
@@ -70,15 +98,15 @@ func TestIncorrectLengthReturnsError(t *testing.T) {
 }
 
 func TestDigitEightReturnedFromComponantStrings(t *testing.T) {
-	input := [3]string { " _ ", "|_|", "|_|"}
+	input := []string{" _ ", "|_|", "|_|"}
 	result := parseDigit(input)
-	assert.Equal(t, '8', result)
+	assert.Equal(t, "8", result)
 }
 
 func TestDigitFourReturnedFromComponantStrings(t *testing.T) {
-	input := [3]string { "   ", "|_|", "  |"}
+	input := []string{"   ", "|_|", "  |"}
 	result := parseDigit(input)
-	assert.Equal(t, '4', result)
+	assert.Equal(t, "4", result)
 }
 
 // TODO: Parse digits
